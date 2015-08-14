e9b87f97061892ca81d212919f256f61750299e5
David J. Brenes <davidjbrenes@gmail.com>
2015-08-14 14:13:51 +0200

    Code commented

--- app/models/acts_as_constrained/concerns/constrained.rb
+++ app/models/acts_as_constrained/concerns/constrained.rb
@@ -1,18 +1,66 @@
 module ActsAsConstrained::Concerns
 
+  ##
+  # This is the general Constrained concern that will add the method to include
+  # more constraints and a scope to constrain using a different set of
+  # constraints
+  #
+  # To use it in a model you just have to include the concern and call
+  # constrain_by with different options:
+  #
+  # class MyConstrainedModel < ActiveRecord:.Base
+  #
+  #   include ActsAsConstrained::Concerns::Constrained
+  #
+  #   constrain_by :date
+  #
+  # end
+  #
+  # And then you can call obtain all your models filtered by date:
+  #
+  #    MyConstrainedModel.constrained_by date: Date.tomorrow
+  #
+  # Notice that actual constraining behaviour (with scopes and all that stuff)
+  # is not defined in this concern, but in the specific constraint concern (e.g.
+  # see ActsAsConstrained::Cocnerns::DateConstraint for the :date constraint)
   module Constrained
 
     extend ActiveSupport::Concern
 
     included do
 
+      ##
+      # This method just includes the Constraint concern into this class and all
+      # the behaviour is delegated to that concern.
       def self.constrain_by constraint_type, options = {}
-        self.include "ActsAsConstrained::Concerns::#{constraint_type.to_s.camelize}Constraint".constantize
+        constraint_name = constraint_type.to_s.camelize
+        self.include "ActsAsConstrained::Concerns::#{constraint_name}Constraint".constantize
       end
 
+      ##
+      # General constrained scope. It takes all the constraints you want to
+      # apply and creates a chain of scopes.
+      #
+      # Those scopes must be provided by each concern that is included by the
+      # 'constrain_by' method.
+      #
+      # Constraints are passed as a hash to the scope with the key being the
+      # kind of constraint being applied and the value the params passed to the
+      # constraining scope.
+      #
+      # As an example, calling:
+      #
+      # MyConstrainedModel.constrained_by date: Date.tomorrow, dummy: 99
+      #
+      # would be equivalent to:
+      #
+      # MyConstrainedModel.constrained_by_date(Date.tomorrow).
+      #   constrained_by_dummy(99)
       scope :constrained_by, ->(constraints) do
         constraints.inject(self) { |result, constraint|
+          # We get the constraint kinds and params
           constraint_kind, constraint_values = constraint
+          # Now we chain a new scope using the constraint_values as params
           result.send "constrained_by_#{constraint_kind}", constraint_values
         }
       end
--- app/models/acts_as_constrained/concerns/date_constraint.rb
+++ app/models/acts_as_constrained/concerns/date_constraint.rb
@@ -1,12 +1,30 @@
 module ActsAsConstrained::Concerns
+
+  ##
+  # This concern uses the ActsAsConstrained::DateConstraint to create multiple
+  # date constraints over a model.
+  #
+  # This is used when you want to have a model enabled only in certain periods
+  # of time.
+  #
+  # Notice that this concern is used when the constrained object may be active
+  # in multiple periods of time. If you only want to allow one period of time
+  # you should consider using the SingleDateConstraint as it stores the info in
+  # the same table and prevents joins between tables.
   module DateConstraint
 
     extend ActiveSupport::Concern
 
     included do
 
+      ##
+      # Polymorphic relationship between the constrained model and the date
+      # constraints
       has_many :date_constraints, as: :constrained, class_name: "ActsAsConstrained::DateConstraint"
 
+      ##
+      # Scope that performs the joins with the date_constraints table and
+      # filters the dates
       scope :constrained_by_date, ->(time = Time.now) do
         joins(:date_constraints).where(
           "? BETWEEN
