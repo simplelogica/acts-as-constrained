6bf7a034cdca0d34e754e9882aa5942e4b4979ce
David J. Brenes <davidjbrenes@gmail.com>
2015-09-14 17:54:05 +0200

    Fixed the single_date generator when used within an engine
    When used within an engine class_name returns the namespace and breaks the whole migration generator

--- lib/generators/acts_as_constrained/single_date_migration_generator.rb
+++ lib/generators/acts_as_constrained/single_date_migration_generator.rb
@@ -1,5 +1,5 @@
 class ActsAsConstrained::SingleDateMigrationGenerator < Rails::Generators::NamedBase
   def create_migrations
-    generate "migration", "AddSingleDateConstraintTo#{class_name} single_date_constraint_starts_at:date single_date_constraint_ends_at:date"
+    generate "migration", "AddSingleDateConstraintTo#{name.camelize} single_date_constraint_starts_at:date single_date_constraint_ends_at:date"
   end
 end
